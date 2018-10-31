a96e317c1dec61ee66bdb11122c18cdea776baa6
Luca Longinotti <luca.longinotti@inivation.com>
2018-10-31 11:54:01 +0100

    FocalLength calculator: add 2.8mm lens option, add LFOV distances of 2m and 4m. Fix precision of fp output.

--- Hardware user guides/focallen.cpp
+++ Hardware user guides/focallen.cpp
@@ -6,8 +6,8 @@
 static double afov(double width, double focalLength);
 static double lfov(double width, double focalLength, double distanceCM);
 
-static const size_t sizeFocalLengths = 5;
-static const double focalLengths[sizeFocalLengths] = { 2.1, 3.5, 4.5, 6.0, 12.0 };
+static const size_t sizeFocalLengths = 6;
+static const double focalLengths[sizeFocalLengths] = { 2.1, 2.8, 3.5, 4.5, 6.0, 12.0 };
 
 // Angular Field Of View
 static double afov(double width, double focalLength) {
@@ -33,7 +33,8 @@ int main(int argc, char **argv) {
 	auto height = (resVertical * pixelPitch) / 1000.0;
 	auto diagonal = std::sqrt((width * width) + (height * height));
 
-	std::cout.precision(3);
+	std::cout.precision(2);
+	std::cout << std::fixed;
 
 	std::cout << "Width (mm): " << width << std::endl;
 	std::cout << "Height (mm): " << height << std::endl;
@@ -47,6 +48,8 @@ int main(int argc, char **argv) {
 		std::cout << focalLengths[i] <<  "mm lens -> Horizontal Linear FoV at 10cm (cm): " << lfov(width, focalLengths[i], 10.0) << std::endl;
 		std::cout << focalLengths[i] <<  "mm lens -> Horizontal Linear FoV at 30cm (cm): " << lfov(width, focalLengths[i], 30.0) << std::endl;
 		std::cout << focalLengths[i] <<  "mm lens -> Horizontal Linear FoV at 100cm (cm): " << lfov(width, focalLengths[i], 100.0) << std::endl;
+		std::cout << focalLengths[i] <<  "mm lens -> Horizontal Linear FoV at 200cm (cm): " << lfov(width, focalLengths[i], 200.0) << std::endl;
+		std::cout << focalLengths[i] <<  "mm lens -> Horizontal Linear FoV at 400cm (cm): " << lfov(width, focalLengths[i], 400.0) << std::endl;
 	}
 
 	return (EXIT_SUCCESS);
